92c07d5317ea6dbac9db2d4e7cf79095081bd455
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-07-31 15:58:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-class President{
-    constructor(name, pp, yrs, homestate){
-        this.name = name;
-        this.politicalParty = pp;
-        this.yearsInOffice = yrs;
-        this.homeState = homestate;
-    }
+function BoardMember(name, homeState, training){
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
 
-    veto(){
-        return `NO!`
-    };
+  BoardMember.prototype.veto = function(){
+    return 'No, I must disagree'
+  }
 
-    passBill(){
-        return `You can do that!`;
-    };
+  BoardMember.prototype.approve = function(){
+    return 'You can do that!'
+  }
 
-    doCharity(){
-        return `I like to help people.`;
-    };
+  BoardMember.prototype.doCharity = function(){
+    return "I like to help people."
+  }
 
-    conductPressInterview(){
-        return `I am proud to be an American.`;
-    };
+  BoardMember.prototype.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+  }
 
-    sayHi(){
-        return `Hi, my name is ${this.name}. I am from ${this.homeState}. I represent the ${this.politicalParty}s, and was in office ${this.yearsInOffice}.`
-    };
-} 
+  BoardMember.prototype.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  } 
